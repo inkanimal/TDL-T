b31241fd603f62685dd6fdf1c3956bfcd05bf0c9
inkanimal <theatomproject@gmail.com>
2019-01-09 18:27:43 -0500

    update routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+
+  default_scope { where(active: true) }
+
+  validates_presence_of :name
+  validates_presence_of :price
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  resource :categories, only: [:show]
+  resources :products
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
