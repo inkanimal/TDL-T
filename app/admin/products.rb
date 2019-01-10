4df7c8a8f96c9b6ddd4074ef86786934b0f75b9a
inkanimal <theatomproject@gmail.com>
2019-01-09 19:00:19 -0500

    user views created

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,4 +1,30 @@
 ActiveAdmin.register Product do
+  menu :priority => 2
+  permit_params :brand, :name, :description, :price,:featured, :image_file_name, :category_ids => []
+
+  scope :all, :default => true
+
+  scope :active_products do |products|
+    products.where(:active => true)
+  end
+
+  form html: { multipart: true } do |f|
+
+    f.inputs "Product | New" do
+      f.input :brand
+      f.input :name
+      f.input :categories, as: :select, :collection => Category.all.collect {|category| [category.name, category.id] }
+      f.input :price, :sortable => :price do |p|
+        number_to_currency p.price
+      end
+      f.input :image_file_name
+      f.input :description
+      f.input :active
+
+    end
+  f.actions
+end
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resource :categories, only: [:show]
   resources :products
   devise_for :admin_users, ActiveAdmin::Devise.config
