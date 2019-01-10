a09966fade36212f100ea689676717a29271830f
inkanimal <theatomproject@gmail.com>
2019-01-09 19:21:32 -0500

    add products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < InheritedResources::Base
+  def index
+    @products = Product.all
+    @order_item = current_order.order_items.new
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    @order_item = current_order.order_items.new
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:brand, :name, :description, :price, :image_file_name)
+    end
+end
